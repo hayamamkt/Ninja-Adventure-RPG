bc5f0ea17b30364c70b5e8f76d4ef40e44d5b188
hayama <hayamaroom@gmail.com>
2025-04-05 22:22:55 +0800

    add player spaswner

--- globals/player_manager.gd
+++ globals/player_manager.gd
@@ -9,7 +9,7 @@ const PLAYER = preload("res://player/player.tscn")
 
 #var interact_handled : bool = true
 var player : Player
-#var player_spawned : bool = false
+var player_spawned : bool = false
 
 #var level_requirements = [ 0, 50, 100, 200, 400, 800, 1500, 3000, 6000, 12000, 2500 ]
 #var level_requirements = [ 0, 5, 10, 20, 40 ]
@@ -18,9 +18,21 @@ var player : Player
 func _ready() -> void:
 	add_player_instance()
 	await get_tree().create_timer(0.2).timeout
-	#player_spawned = true
+	player_spawned = true
 
 func add_player_instance() -> void:
 	player = PLAYER.instantiate()
 	add_child( player )
-	pass
+
+func set_as_parent(node: Node2D):
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+	node.add_child(player)
+
+func set_player_position(new_pos: Vector2) -> void:
+	print("PlayerMangge: set_player_position=", new_pos)
+	player.global_position = new_pos
+	player.velocity = Vector2.ZERO
+
+func unparent_player(node: Node2D):
+	node.remove_child(player)
