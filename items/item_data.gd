9d9267b23cdcd7d760747c312eaba0e3fe3bb1fe
hayama <hayamaroom@gmail.com>
2025-04-06 16:09:44 +0800

    add item data and inventory data

---
+++ items/item_data.gd
@@ -0,0 +1,17 @@
+extends Resource
+class_name ItemData
+
+@export var name := ""
+@export_multiline var desccriptio := ""
+@export var texture: Texture2D
+
+@export_category("Item Use Effects")
+#@export var effects: Array[ItemEffect]
+
+#func use() -> bool:
+	#if effects.size() == 0: return false
+#
+	#for e in effects:
+		#if e: e.use()
+#
+	#return true
